002f71b729d3ce9cf6d683cd51fbfb6365e733c0
Zhuofu.Jiang <15988841761@163.com>
2024-07-03 20:06:05 +0800

    add session

---
+++ src/main/java/com/atguigu/IndexServlet.java
@@ -0,0 +1,32 @@
+package com.atguigu;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/")
+public class IndexServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String user = (String)req.getSession().getAttribute("user");
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setHeader("X-Powered-By", "JavaEE Servlet");
+        PrintWriter out = resp.getWriter();
+        out.write("<h1>Welcome, " + (user != null ? user : "Guest") + "</h1>");
+        if (user == null) {
+            // 未登录，显示登录链接:
+            out.write("<p><a href=\"/signin\">Sign In</a></p>");
+        } else {
+            // 已登录，显示登出链接:
+            out.write("<p><a href=\"/signout\">Sign Out</a></p>");
+        }
+        out.flush();
+    }
+}
